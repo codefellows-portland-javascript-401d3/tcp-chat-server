f49b52e140eac69035205b83c241e9048622ba45
Danielle H <daheber@gmail.com>
2016-07-18 14:21:59 -0700

    client management encapsulation

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const net = require('net');
 
+class ChatSession {
+  constructor(){
+    this.clients = [];
+  }
+  add(client){
+    this.clients.push(client);
+  }
+  remove(client){
+    const index = this.clients.indexOf(client);
+    if ( index !== -1 ) this.clients.splice( index, 1 );
+    console.log(`${client.name} left the chat session`);
+  }
+  message(client, message){
+    this.clients.forEach(c=>{
+      //if(c === client) return;
+      c.write(`${client.name}: ${message}\n`);
+    });
+  }
+}
+
 let id = 1;
-const clients = [];
+const chatSession = new ChatSession();
 
 const server = net.createServer(client =>{
-  clients.push(client);
+  chatSession.add(client);
+
   client.name = `guest ${id++}`;
   client.setEncoding( 'utf-8' );
 
@@ -13,16 +34,11 @@ const server = net.createServer(client =>{
   console.log(`${client.name} joined the chat session`);
 
   client.on('data', message=>{
-    clients.forEach(c=>{
-      //if(c === client) return;
-      c.write(`${client.name}: ${message}\n`);
-    });
+    chatSession.message(client, message);
   });
 
   client.on('close', ()=>{
-    const index = clients.indexOf(client);
-    if ( index !== -1 ) clients.splice( index, 1 );
-    console.log(`${client.name} left the chat session`);
+    chatSession.remove(client);
   });
 
 });
