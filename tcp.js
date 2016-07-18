7acc69a83a7b3c5c147bdec059d6b69437d446e9
Arielle Foldoe <afoldoe@gmail.com>
2016-07-18 12:58:25 -0700

    Added basic chat-ish functionality

--- tcp.js
+++ tcp.js
@@ -1,14 +1,45 @@
 const net = require('net');
 
-const server = new.createServer(socket => {
-  consle.log('Hello form the server');
+let i = 0;
+
+class Broadcast {
+  constructor() {
+    this.clients = [];
+  }
+  add(client) {
+    this.clients.push(client);
+  }
+  remove(client) {
+    const index = this.clients.indexOf(client);
+    if(index !== -1) this.clients.splice(index, 1);
+  }
+  send(client, msg) {
+    this.clients.forEach(c => {
+      if(c === client) return;
+      c.write(`${client.name} says: ${msg}`); 
+    });
+  }
+}
+
+const broadcast = new Broadcast();
+
+const server = net.createServer(client => {
+  broadcast.add(client);
+  client.name = `name ${i++}`;
+  client.setEncoding(`utf-8`);
+  
+  client.on(`data`, data => {
+    broadcast.send(client, data);
+  });
+  
+  client.on('close', () => {
+    broadcast.remove(client);
+  });
 });
 
-server.on('close', () => {
-  console.log('Server closed!');
-});
 
-server.listen(65000, () => {
+
+server.listen(65500, () => {
   address = server.address();
   console.log('opened server on', address);
 });
