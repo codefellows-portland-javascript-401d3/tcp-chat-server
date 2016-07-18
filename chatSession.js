77c183236c5df403a9620f806c8366cf176b69d5
Danielle H <daheber@gmail.com>
2016-07-18 14:50:46 -0700

    refactored class to use prototypes instead

--- chatSession.js
+++ chatSession.js
@@ -1,18 +1,40 @@
-module.exports = class ChatSession {
-  constructor(){
-    this.clients = [];
-  }
-  add(client){
-    this.clients.push(client);
-  }
-  remove(client){
-    const index = this.clients.indexOf(client);
-    if ( index !== -1 ) this.clients.splice( index, 1 );
-    console.log(`${client.name} left the chat session`);
-  }
-  message(client, message){
-    this.clients.forEach(c=>{
-      c.write(`${client.name}: ${message}\n`);
-    });
-  }
+// module.exports = class ChatSession {
+//   constructor(){
+//     this.clients = [];
+//   }
+//   add(client){
+//     this.clients.push(client);
+//   }
+//   remove(client){
+//     const index = this.clients.indexOf(client);
+//     if ( index !== -1 ) this.clients.splice( index, 1 );
+//     console.log(`${client.name} left the chat session`);
+//   }
+//   message(client, message){
+//     this.clients.forEach(c=>{
+//       c.write(`${client.name}: ${message}\n`);
+//     });
+//   }
+// };
+
+function ChatSession(){
+  this.clients = new Array();
+}
+
+ChatSession.prototype.add = function(client){
+  this.clients.push(client);
 };
+
+ChatSession.prototype.remove = function(client){
+  const index = this.clients.indexOf(client);
+  if ( index !== -1 ) this.clients.splice( index, 1 );
+  console.log(`${client.name} left the chat session`);
+};
+
+ChatSession.prototype.message = function(client, message){
+  this.clients.forEach(c=>{
+    c.write(`${client.name}: ${message}\n`);
+  });
+};
+
+module.exports = ChatSession;
