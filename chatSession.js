ed16b5c9c40186506f6f736faec27d9278911c52
Danielle H <daheber@gmail.com>
2016-07-18 19:54:49 -0700

    messaging to users when someone joins and leaves updated

--- chatSession.js
+++ chatSession.js
@@ -2,14 +2,34 @@ function ChatSession(){
   this.clients = new Array();
 }
 
+ChatSession.prototype.name = function(client){
+  client.name = 'guest ' + Math.floor(Math.random()*5000);
+};
+
+ChatSession.prototype.rename = function(client){
+  client.name = 'guest ' + Math.floor(Math.random()*5000);
+};
+
 ChatSession.prototype.add = function(client){
   this.clients.push(client);
+  this.clients.forEach(c=>{
+    if(c === client){
+      return;
+    } else {
+      c.write(`\n${client.name} joined the chat.\n`);
+    }
+
+  });
 };
 
 ChatSession.prototype.remove = function(client){
   const index = this.clients.indexOf(client);
   if ( index !== -1 ) this.clients.splice( index, 1 );
-  console.log(`${client.name} left the chat session`);
+
+  this.clients.forEach(c=>{
+    c.write(`${client.name} has left the chat session.\n`);
+  });
+
 };
 
 ChatSession.prototype.message = function(client, message){
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const net = require('net');
 const ChatSession = require('./chatSession');
 
-let id = 1;
 const chatSession = new ChatSession();
 
 const server = net.createServer(client =>{
+  chatSession.name(client);
   chatSession.add(client);
 
-  client.name = `guest ${id++}`;
   client.setEncoding( 'utf-8' );
 
   client.write(`Welcome to the chat session ${client.name}!\n`);
-  console.log(`${client.name} joined the chat session`);
 
   client.on('data', message=>{
+    //if the message contains \nick new-name
+      //create a new nick name
+      //broadcast to everyone that [old name] is now [new name]
     chatSession.message(client, message);
+
   });
 
   client.on('close', ()=>{
