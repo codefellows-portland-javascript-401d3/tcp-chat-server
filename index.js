e0ead1d333f3fb2d33001ffcf6ed14aac9d65e25
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-07-19 08:32:21 -0700

    names work with new method

--- index.js
+++ index.js
@@ -4,28 +4,11 @@ const clientList = require('./lib/clientList');
 const server = net.createServer( socket => {
   console.log('Socket Connected!');
   socket.setEncoding('utf8');
-  let tempName = 'guest-' + new Date().getTime();
-  
-  let clientName = '';
-  clientList.init(socket, tempName);
+
+  clientList.init(socket);
 
   socket.on('data', data => {
-    // Parse data to ignore null strings and unwanted characters.
-    let input = data.toString().replace(/[\r\n]/g, ''); // potentially remove all unwanted values here
-    
-    if(!clientName) {
-      // First entry is used as client's name
-      if (input) {
-        clientName = input;
-        clientList.sendAll(socket, clientName + ' has joined.\n');
-      } else {
-        // Repeat name query if empty string was sent.
-        clientList.sendTo(socket, 'What is your name? ');
-      }
-    } else {
-      // Normal broadcast output
-      if (input) clientList.sendAll(socket, clientName + ' says: ' + data); 
-    }
+    clientList.send(socket,data); 
   });
 
   socket.on('close', () => {
